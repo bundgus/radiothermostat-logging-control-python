35d7806300122df69dd122990168d46cc0b90eb2
bundgus <bundgus@gmail.com>
2017-11-11 13:18:35 -0600

    updating auto set temp

--- analytics/tstat_pipeline.py
+++ analytics/tstat_pipeline.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:0.9918115370805181
-exported_pipeline = ExtraTreesClassifier(bootstrap=False, criterion="gini", max_features=0.55, min_samples_leaf=20, min_samples_split=18, n_estimators=100)
+# Score on the training set was:0.9918594068745439
+exported_pipeline = RandomForestClassifier(bootstrap=False, criterion="gini", max_features=0.45, min_samples_leaf=18, min_samples_split=5, n_estimators=100)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
--- radiotherm-auto-set-temp.py
+++ radiotherm-auto-set-temp.py
@@ -9,22 +9,34 @@ IP1 = '192.168.0.111'
 # 2ND floor
 IP2 = '192.168.0.112'
 
-max_samples_under_target = 8
-seconds_in_period = 300  # 5 minutes (600 seconds)
+max_samples_over_under_target = 8
+minutes_in_period = 5
 #########################################
 
-def get_target_temp(tstat):
+
+def get_target_temp_cooling(tstat):
     now = datetime.datetime.now()
     #print('getting target temp ', tstatnum, now)
     #print(now.isoweekday())
     #print(now.hour)
     return 75
 
+
+def get_target_temp_heating(tstat):
+    now = datetime.datetime.now()
+    #print('getting target temp ', tstatnum, now)
+    #print(now.isoweekday())
+    #print(now.hour)
+    return 70
+
+
 periods_under_target_1 = 0
 periods_under_target_2 = 0
+periods_over_target_1 = 0
+periods_over_target_2 = 0
 
 print('starting auto set temp')
-print('max undertemp time (mins): ', (max_samples_under_target+1)*seconds_in_period/60)
+print('max undertemp time (mins): ', (max_samples_over_under_target + 1) * minutes_in_period)
 while True:
     try:
         tstat1 = radiotherm.get_thermostat(host_address=IP1)
@@ -33,31 +45,43 @@ while True:
         while True:
             now = datetime.datetime.now(timezone.utc)
 
-            target_temp_1 = get_target_temp(tstat1)
-            if tstat1.tmode['human'] == 'Cool' and tstat1.t_cool['raw'] < target_temp_1:
-                if tstat1.t_cool['raw'] < (target_temp_1 - 1):
-                    tstat1.t_cool = target_temp_1 - 1
-                print(now, 'hvac 1 set to less than 75 for ' + str(periods_under_target_1 * seconds_in_period / 60)
-                      + ' minutes')
-                if periods_under_target_1 > max_samples_under_target:
-                    print(now, 'setting hvac 1 to target temp ' + str(target_temp_1))
-                    tstat1.t_cool = target_temp_1
+            target_cool_temp_1 = get_target_temp_cooling(tstat1)
+            target_cool_temp_2 = get_target_temp_cooling(tstat2)
+            target_heat_temp_1 = get_target_temp_heating(tstat1)
+            target_heat_temp_2 = get_target_temp_heating(tstat2)
+
+            if tstat1.tmode['human'] == 'Cool' and tstat1.t_cool['raw'] < target_cool_temp_1:
+                if tstat1.t_cool['raw'] < (target_cool_temp_1 - 1):
+                    tstat1.t_cool = target_cool_temp_1 - 1
+                print(now, 'hvac 1 cooling set to less than ' + str(target_cool_temp_1) + 'for '
+                      + str(periods_under_target_1 * minutes_in_period) + ' minutes')
+                if periods_under_target_1 > max_samples_over_under_target:
+                    print(now, 'setting hvac 1 cooling to target temp ' + str(target_cool_temp_1))
+                    tstat1.t_cool = target_cool_temp_1
                     periods_under_target_1 = 0
                 else:
                     periods_under_target_1 += 1
+            elif tstat1.tmode['human'] == 'Heat' and tstat1.t_heat['raw'] > target_heat_temp_1:
+                print(now, 'hvac 1 set to more than' + target_heat_temp_1 + 'for '
+                      + str(periods_over_target_1 * minutes_in_period) + ' minutes')
+                if periods_over_target_1 > max_samples_over_under_target:
+                    print(now, 'setting hvac 1 heating to target temp ' + str(target_heat_temp_1))
+                    tstat1.t_heat = target_heat_temp_1
+                    periods_over_target_1 = 0
+                else:
+                    periods_over_target_1 += 1
             else:
                 pass
                 # print(now, 'ac 1 not set under 75')
 
-            target_temp_2 = get_target_temp(2)
-            if tstat2.tmode['human'] == 'Cool' and tstat2.t_cool['raw'] < target_temp_2:
-                if tstat2.t_cool['raw'] < (target_temp_2 - 1):
-                    tstat2.t_cool = target_temp_2 - 1
-                print(now, 'hvac 2 set to less than 75 for ' + str(periods_under_target_2 * seconds_in_period / 60)
+            if tstat2.tmode['human'] == 'Cool' and tstat2.t_cool['raw'] < target_cool_temp_2:
+                if tstat2.t_cool['raw'] < (target_cool_temp_2 - 1):
+                    tstat2.t_cool = target_cool_temp_2 - 1
+                print(now, 'hvac 2 cooling set to less than 75 for ' + str(periods_under_target_2 * minutes_in_period)
                       + ' minutes')
-                if periods_under_target_2 > max_samples_under_target:
-                    print(now, 'setting hvac 2 to target temp ' + str(target_temp_2))
-                    tstat2.t_cool = target_temp_2
+                if periods_under_target_2 > max_samples_over_under_target:
+                    print(now, 'setting hvac 2 cooling to target temp ' + str(target_cool_temp_2))
+                    tstat2.t_cool = target_cool_temp_2
                     periods_under_target_2 = 0
                 else:
                     periods_under_target_2 += 1
@@ -65,7 +89,7 @@ while True:
                 pass
                 # print(now, 'ac 2 not set under 75')
 
-            time.sleep(seconds_in_period)
+            time.sleep(minutes_in_period * 60)
     except Exception as e:
         print('exception occurred', e)
         time.sleep(300)  # Delay for 5 minutes (300 seconds)
