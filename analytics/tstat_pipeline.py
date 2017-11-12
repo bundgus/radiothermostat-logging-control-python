7ad444b7f77c0e2b6b426b29a84c35192b8bf717
bundgus <bundgus@gmail.com>
2017-11-11 20:48:13 -0600

    updated control, optimized prediction with tpot and xgboost

--- analytics/tstat_pipeline.py
+++ analytics/tstat_pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:0.9918594068745439
-exported_pipeline = RandomForestClassifier(bootstrap=False, criterion="gini", max_features=0.45, min_samples_leaf=18, min_samples_split=5, n_estimators=100)
+# Score on the training set was:0.9910932267287471
+exported_pipeline = XGBClassifier(learning_rate=0.001, max_depth=5, min_child_weight=7, n_estimators=100, nthread=1, subsample=0.7500000000000001)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
--- radiotherm-auto-set-temp.py
+++ radiotherm-auto-set-temp.py
@@ -14,20 +14,26 @@ minutes_in_period = 5
 #########################################
 
 
-def get_target_temp_cooling(tstat):
-    now = datetime.datetime.now()
-    #print('getting target temp ', tstatnum, now)
-    #print(now.isoweekday())
-    #print(now.hour)
+def get_target_temp_cooling(tstat, story=None):
     return 75
 
 
-def get_target_temp_heating(tstat):
+def get_target_temp_heating(tstat, story=None):
     now = datetime.datetime.now()
-    #print('getting target temp ', tstatnum, now)
     #print(now.isoweekday())
-    #print(now.hour)
-    return 70
+    hour_of_day = now.hour
+    print('current hour', hour_of_day)
+    if story is 1:
+        # between 5 am and 10 pm turn up the heat downstairs
+        if 5 < hour_of_day <= 7:
+            return 72
+        elif 7 < hour_of_day < 22:
+            return 70
+        else:
+            return 68
+    # upstairs always has the heat turned down
+    else:
+        return 62
 
 
 periods_under_target_1 = 0
@@ -45,27 +51,27 @@ while True:
         while True:
             now = datetime.datetime.now(timezone.utc)
 
-            target_cool_temp_1 = get_target_temp_cooling(tstat1)
-            target_cool_temp_2 = get_target_temp_cooling(tstat2)
-            target_heat_temp_1 = get_target_temp_heating(tstat1)
-            target_heat_temp_2 = get_target_temp_heating(tstat2)
+            target_cool_temp_1 = get_target_temp_cooling(tstat1, story=1)
+            target_cool_temp_2 = get_target_temp_cooling(tstat2, story=2)
+            target_heat_temp_1 = get_target_temp_heating(tstat1, story=1)
+            target_heat_temp_2 = get_target_temp_heating(tstat2, story=2)
 
             if tstat1.tmode['human'] == 'Cool' and tstat1.t_cool['raw'] < target_cool_temp_1:
                 if tstat1.t_cool['raw'] < (target_cool_temp_1 - 1):
                     tstat1.t_cool = target_cool_temp_1 - 1
-                print(now, 'hvac 1 cooling set to less than ' + str(target_cool_temp_1) + 'for '
+                print(now, 'ac 1 cooling set to less than ' + str(target_cool_temp_1) + ' for '
                       + str(periods_under_target_1 * minutes_in_period) + ' minutes')
                 if periods_under_target_1 > max_samples_over_under_target:
-                    print(now, 'setting hvac 1 cooling to target temp ' + str(target_cool_temp_1))
+                    print(now, 'setting ac 1 cooling to target temp ' + str(target_cool_temp_1))
                     tstat1.t_cool = target_cool_temp_1
                     periods_under_target_1 = 0
                 else:
                     periods_under_target_1 += 1
             elif tstat1.tmode['human'] == 'Heat' and tstat1.t_heat['raw'] > target_heat_temp_1:
-                print(now, 'hvac 1 set to more than' + target_heat_temp_1 + 'for '
+                print(now, 'heat 1 set to more than ' + str(target_heat_temp_1) + ' for '
                       + str(periods_over_target_1 * minutes_in_period) + ' minutes')
                 if periods_over_target_1 > max_samples_over_under_target:
-                    print(now, 'setting hvac 1 heating to target temp ' + str(target_heat_temp_1))
+                    print(now, 'setting heat 1 heating to target temp ' + str(target_heat_temp_1))
                     tstat1.t_heat = target_heat_temp_1
                     periods_over_target_1 = 0
                 else:
@@ -77,14 +83,23 @@ while True:
             if tstat2.tmode['human'] == 'Cool' and tstat2.t_cool['raw'] < target_cool_temp_2:
                 if tstat2.t_cool['raw'] < (target_cool_temp_2 - 1):
                     tstat2.t_cool = target_cool_temp_2 - 1
-                print(now, 'hvac 2 cooling set to less than 75 for ' + str(periods_under_target_2 * minutes_in_period)
+                print(now, 'ac 2 cooling set to less than 75 for ' + str(periods_under_target_2 * minutes_in_period)
                       + ' minutes')
                 if periods_under_target_2 > max_samples_over_under_target:
-                    print(now, 'setting hvac 2 cooling to target temp ' + str(target_cool_temp_2))
+                    print(now, 'setting ac 2 cooling to target temp ' + str(target_cool_temp_2))
                     tstat2.t_cool = target_cool_temp_2
                     periods_under_target_2 = 0
                 else:
                     periods_under_target_2 += 1
+            elif tstat2.tmode['human'] == 'Heat' and tstat2.t_heat['raw'] > target_heat_temp_2:
+                print(now, 'heat 2 set to more than ' + str(target_heat_temp_2) + ' for '
+                      + str(periods_over_target_2 * minutes_in_period) + ' minutes')
+                if periods_over_target_2 > max_samples_over_under_target:
+                    print(now, 'setting heat 2 heating to target temp ' + str(target_heat_temp_2))
+                    tstat2.t_heat = target_heat_temp_2
+                    periods_over_target_2 = 0
+                else:
+                    periods_over_target_2 += 1
             else:
                 pass
                 # print(now, 'ac 2 not set under 75')
